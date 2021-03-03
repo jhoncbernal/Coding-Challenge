4434f372184a32d051ee8760b83888b0a1cd5222
jhoncbernal <jhoncbernal@gmail.com>
2021-03-03 01:54:52 -0500

    [MOD]:add sync logs to the response

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,9 @@
 "use strict";
-
+const _ = require("lodash");
 // Print all entries, across all of the sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+module.exports = (logSources, sortLogArray) => {
+  let logInformationArray = logSources.map((source) => source.pop());
+  sortLogArray(logInformationArray);
   return console.log("Sync sort complete.");
 };
